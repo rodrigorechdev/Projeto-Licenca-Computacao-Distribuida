7a779835145a79d060e2f2bdae96c123e9257efa
Rodrigo Rech <rodrigorech@hotmail.com>
2022-03-11 20:19:49 -0300

    projeto base server
    base

---
+++ servidor/licenca/src/main/java/com/licenca/licenca/LicencaApplication.java
@@ -0,0 +1,13 @@
+package com.licenca.licenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicencaApplication.class, args);
+	}
+
+}
---
+++ servidor/licenca/src/test/java/com/licenca/licenca/LicencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licenca.licenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
